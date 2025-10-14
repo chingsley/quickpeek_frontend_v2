3047295b3824e5f4e59b025190cb61be9477852f
Kingsley Eneja <kingsley.eneja@getir.com>
2025-10-14 17:44:02 -0300

    Implement app loader

--- store/app.store.ts
+++ store/app.store.ts
@@ -1,1 +1,21 @@
-// for app - wide state
+import { create } from "zustand";
+
+interface AppState {
+  loading: boolean;
+  error: string | null;
+  message: string | null;
+  setLoading: (loading: boolean) => void;
+  setError: (error: string | null) => void;
+  setMessage: (message: string | null) => void;
+}
+
+const useAppStore = create<AppState>((set) => ({
+  loading: false,
+  error: null,
+  message: null,
+  setLoading: (loading) => set({ loading }),
+  setError: (error) => set({ error }),
+  setMessage: (message) => set({ message }),
+}));
+
+export default useAppStore;
